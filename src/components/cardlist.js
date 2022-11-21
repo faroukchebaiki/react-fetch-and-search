cbc8e720c8634dfb33c70fa467c33b415ce6b8a3
Farouk Chebaiki <farouk.chebaiki@gmail.com>
2022-11-21 22:59:30 +0100

    updating cardlist

--- src/components/cardlist.js
+++ src/components/cardlist.js
@@ -1,7 +1,14 @@
 import Card from "./card";
 export default function Cardlist() {
   return (
-    <div>
+    <div className="columns is-1 is-variable" style={{ height: "auto;" }}>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
       <Card />
     </div>
   );
