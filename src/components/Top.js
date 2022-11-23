41981bf1758f06ac59c1066049ebdc463e6fd7c4
Farouk Chebaiki <f7484r12@gmail.com>
2022-11-23 22:58:27 +0100

    width ajusment for cardlist

--- src/components/Top.js
+++ src/components/Top.js
@@ -1,7 +1,7 @@
 import Search from "./search";
 export default function Top() {
   return (
-    <section className="hero" style={{ borderBottom: "1px solid grey" }}>
+    <section className="hero" style={{ borderBottom: "1px solid white" }}>
       <div className="hero-body">
         <p className="title">Farouk</p>
         <p className="subtitle">this is just fetching and searching</p>
