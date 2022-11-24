8d0571ef9cecd1a624e1c518f159720b9d8bc6fe
Farouk Chebaiki <f7484r12@gmail.com>
2022-11-24 22:38:13 +0100

    adding a dark theme

--- src/components/Top.js
+++ src/components/Top.js
@@ -1,10 +1,10 @@
 import Search from "./search";
 export default function Top() {
   return (
-    <section className="hero" style={{ borderBottom: "1px solid white" }}>
-      <div className="hero-body">
-        <p className="title">Farouk</p>
-        <p className="subtitle">this is just fetching and searching</p>
+    <section className="hero" style={{ borderBottom: "1px solid white"}}>
+      <div className="hero-body" style={{color: "white" }}>
+        <p className="title" style={{color: "white" }}>Farouk</p>
+        <p className="subtitle" style={{color: "white" }}>this is just fetching and searching</p>
         <Search />
       </div>
     </section>
